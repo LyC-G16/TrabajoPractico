0476eb8184b5600f44f3c110d8675bfda13ab295
Joaquín Oria <joaquin.ariel.o@gmail.com>
2020-10-08 21:21:57 -0300

    Se implementaron todas las reglas de bison.

--- Primera entrega/Primera.sh
+++ Primera entrega/Primera.sh
@@ -1,10 +1,9 @@
 flex ./lexico/Lexico.l
 bison -dyv ./sintactico/Sintactico.y
-gcc lex.yy.c y.tab.c -o programa
-./programa ./sintactico/prueba_sintactica.txt
+gcc lex.yy.c y.tab.c -o Primera.exe
+./Primera.exe ./sintactico/prueba_sintactica.txt
 
 rm lex.yy.c
 rm y.tab.c
 rm y.output
 rm y.tab.h
-rm programa
